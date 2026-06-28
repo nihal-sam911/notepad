a2d77f74aeea0b304294071dbfa12e743b3f6285
nihal_sam <nihalsam2009@gmail.com>
2026-06-28 16:01:36 +0530

    Create database.py
    Implement database connection and create function.

---
+++ database.py
@@ -0,0 +1,44 @@
+# connector 
+
+import mysql.connector as m
+
+def connection():
+
+    try:
+        con = m.connect (
+             host="localhost",
+             user="root",
+             password="nihaltalent7",
+             database="notepad"
+             )
+       
+        if con.is_connected() :
+            print("successfully conencted")
+            return con
+
+    except m.Error as e:
+        print(f"connection failed: {e}")
+        return None
+
+             
+
+
+def create():
+
+    con = connection()
+    if con is None:
+        return false
+    cur = con.cusor()
+
+    query = "CREATE IF NOT EXISTS notepad"
+
+    cur.execute(query)
+    cur.close()
+    con.close()
+
+
+
+
+
+    
+
